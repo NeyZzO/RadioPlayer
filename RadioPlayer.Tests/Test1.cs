f5405bd959493343316382c31488f46edd2d62db
Eliott <eliott.dagostinoz@u-night.org>
2026-04-14 13:16:40 +0200

    feat: tray icon + app icon

---
+++ RadioPlayer.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace RadioPlayer.Tests {
+    [TestClass]
+    [DeploymentItem("TestData")]
+    public sealed class Test1 {
+        [TestMethod(DisplayName = "2 + 2 = 4")]
+        [Description("Test de base pour tester les tests unitaires en C#")]
+        public void TestMethod1() {
+            int expected = 4;
+            int actual = 2 + 2;
+            Assert.AreEqual(expected, actual, "2 + 2 ça fait 4");
+        }
+    }
+}
